b75d88622b81084d436eadcd981771dfe2410560
Bartosz Adamczyk <services@bartekadamczyk.eu>
2025-09-25 00:24:08 +0200

    Add files via upload

---
+++ networks.nix
@@ -0,0 +1,29 @@
+{ ... }:
+{
+  homelab.networks = {
+    external = {
+      dadzbog = {
+        address = "192.168.88.122";
+        gateway = "192.168.88.1";
+        interface = "enp1s0";
+      };
+    };
+    local = {
+      lan = {
+        id = 1;
+        cidr = {
+          v4 = "192.168.88.1/24";
+          v6 = null;
+        };
+        interface = "enp1s0";
+        trusted = true;
+        reservations = {
+          dadzbog = {
+            MACAddress = "b8:ae:ed:5f:4c:7c";
+            Address = "192.168.88.122";
+          };
+        };
+      };
+    };
+  };
+}
